52e7ea1c3709168431bbc1fa9a608390f34890fb
Saptadwipa Choudhury <saptadweepa.30@gmail.com>
2024-08-30 13:07:08 +0200

    added files

---
+++ src/test/java/com/ecommerce/AnalyticService/AnalyticServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.AnalyticService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
